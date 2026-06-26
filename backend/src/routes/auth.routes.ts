ec488088acac369dc190d2f9aa32190294b479b0
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-26 23:39:47 +0530

    Update auth.routes.ts

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {registerController , loginController , refreshController} from '../controllers/auth.controller'
+
+const router = Router();
+
+router.post("/register" , registerController )
+
+router.post("/login" , loginController)
+
+router.post("/refresh" , refreshController)
+
+export {router}

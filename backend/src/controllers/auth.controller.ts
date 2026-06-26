c43ff33c4e70958da91ceb32ee6ccd5ea7143079
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-26 22:30:40 +0530

    Update auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,8 +1,9 @@
 import bcrypt from "bcryptjs";
 import prisma from "../utils/prisma";
+import { generateAccessToken, generateRefreshToken, verifyToken } from '../utils/jwt.utils.js'
 
 
-async function register(req: any, res: any) {
+async function registerController(req: any, res: any) {
     try {
 
         const { name, password, email } = req.body
@@ -15,14 +16,55 @@ async function register(req: any, res: any) {
             }
         })
         res.status(201).json({
-            message : "User registered successfully",
-            name : user.name ,
-            email : user.email
+            message: "User registered successfully",
+            name: user.name,
+            email: user.email
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: "Some error occured",
+            error: err
+        })
+    }
+}
+
+
+async function loginController(req: any, res: any) {
+    try {
+        const { email, password } = req.body;
+        const user = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if (!user) {
+            return res.status(400).json({
+                message: "Email does not exists , please register !"
+            })
+        }
+
+        const check = await bcrypt.compare(password, user.password)
+        if (!check) {
+            return res.status(401).json({
+                message: "Invalid Credentials"
+            })
+        }
+        const accessToken = generateAccessToken(user.id)
+        const refreshToken = generateRefreshToken(user.id)
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000 })
+
+        return res.status(200).json({
+            message: "User logged-in successfully",
+            accessToken,
+            email: user.email,
+            name: user.name,
         })
     }catch(err){
         res.status(500).json({
-            message : "Some error occured",
+            message : "Some Error occurred",
             error : err
         })
     }
 }
+
+export {registerController , loginController}

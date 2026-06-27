17763432aa324967fd1846a2f2fb4f66527a6b99
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-27 13:37:46 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import { router } from './routes/auth.routes'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -10,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
+app.use('/api/auth' , router)
 app.get('/health' ,(req,res)=>{
     res.json({
         status:"ok",

b3960af57389449021e03b9510f3583683f7ffb5
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-27 18:42:21 +0530

    Created React Routes for register , login and dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <div className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
-            <div className="text-center">
-              <h1 className="text-4xl font-bold text-purple-400">DevBoard</h1>
-              <p className="text-gray-400 mt-2">Your developer productivity hub</p>
-            </div>
-          </div>
-        } />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
+      
+      
     </BrowserRouter>
   )
 }

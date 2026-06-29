86e5ed9fe4e906928e713c04f144972d62c6b2d2
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-29 19:53:21 +0530

    Add review router

--- backend/src/routes/ai.routes.ts
+++ backend/src/routes/ai.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { VerifyUser } from "../middleware/auth.middleware";
-import { expandTask } from "../controllers/ai.controller";
+import { expandTask, reviewCode } from "../controllers/ai.controller";
+
 const aiRouter = Router()
 
 aiRouter.post('/expand' , VerifyUser ,expandTask)
+
+aiRouter.post('/review' , VerifyUser , reviewCode)
 
 export {aiRouter}

3ef11144ec63006d63aeed0879cd8e954e7462f4
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-30 16:06:50 +0530

    Update ai.routes.ts

--- backend/src/routes/ai.routes.ts
+++ backend/src/routes/ai.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { VerifyUser } from "../middleware/auth.middleware";
 import { expandTask, reviewCode } from "../controllers/ai.controller";
+import { aiRateLimiter } from "../middleware/rateLimit.middleware"; 
 
 const aiRouter = Router()
 
-aiRouter.post('/expand' , VerifyUser ,expandTask)
+aiRouter.post('/expand' ,aiRateLimiter , VerifyUser,expandTask)
 
-aiRouter.post('/review' , VerifyUser , reviewCode)
+aiRouter.post('/review' ,aiRateLimiter, VerifyUser ,reviewCode)
 
 export {aiRouter}

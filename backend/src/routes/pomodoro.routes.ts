c1817ec7d26f9db67c7cccde3c58507fcc29727b
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-30 10:35:16 +0530

    Update pomodoro.routes.ts

--- backend/src/routes/pomodoro.routes.ts
+++ backend/src/routes/pomodoro.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { VerifyUser } from "../middleware/auth.middleware";
-import { createSession } from "../controllers/pomodoro.controller";
+import { createSession, getSession } from "../controllers/pomodoro.controller";
 
 const pomodoroRouter = Router()
 
 pomodoroRouter.post('/create' , VerifyUser , createSession)
+
+pomodoroRouter.get('/' , VerifyUser , getSession)
 
 export {pomodoroRouter}
